80fd88d736f1a580fb0aedc56a373a94d7c3c5ae
DrinE Liu <drine@client.com.au>
2021-09-24 22:27:32 +0800

    Added scheduler

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require "sidekiq-scheduler/web"
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['SIDEKIQ_USER'] && password == ENV['SIDEKIQ_PWD']
+  end
+  mount Sidekiq::Web => '/sidekiq'
+
   root 'coins#index'
 
   resources :coins, only: [:index] do
