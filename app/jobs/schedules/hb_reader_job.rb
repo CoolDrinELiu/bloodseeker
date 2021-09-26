2d64dbc2eb4de7c231c0c2f79a8403c78eb0508b
DrinE Liu <drine@client.com.au>
2021-09-26 14:36:54 +0800

    added logs

--- app/jobs/schedules/hb_reader_job.rb
+++ app/jobs/schedules/hb_reader_job.rb
@@ -3,7 +3,14 @@
 module Schedules
   class HbReaderJob < ApplicationJob
     def perform
-      HbWorker.perform_async
+      log = RakeLog.create(title: "HB Job")
+      begin
+        HbWorker.perform_async
+      rescue Exception => e
+        status = false
+        message = e
+      end
+      log.update_attributes(status: status, message: message)
     end
   end
 end
--- app/workers/hb_worker.rb
+++ app/workers/hb_worker.rb
@@ -4,19 +4,26 @@ class HbWorker
   sidekiq_options retry: 1, backtrace: true
 
   def perform
-    result = JSON.parse Watchcow::Client.call
-    mark = Time.now.to_i
-    result.values.flatten.each do |g|
-      Coin.create(
-        name: g["coin"],
-        days_left: g["days"],
-        annual_rate: g["margin"],
-        landed_rate: g["real_margin_expected"],
-        future_price: g["dm_first_bid"],
-        spot_price: g["spot_first_bid"],
-        gap_rate: g["gap_rate"],
-        birth_mark: mark
-      )
+    log = RakeLog.create(title: "HB Job")
+    begin
+      result = JSON.parse Watchcow::Client.call
+      mark = Time.now.to_i
+      result.values.flatten.each do |g|
+        Coin.create(
+          name: g["coin"],
+          days_left: g["days"],
+          annual_rate: g["margin"],
+          landed_rate: g["real_margin_expected"],
+          future_price: g["dm_first_bid"],
+          spot_price: g["spot_first_bid"],
+          gap_rate: g["gap_rate"],
+          birth_mark: mark
+        )
+      end
+    rescue Exception => e
+      status = false
+      message = e
     end
+    log.update_attributes(status: status, message: message)
   end
 end
